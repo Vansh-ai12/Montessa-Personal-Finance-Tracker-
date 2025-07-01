72ffd4cbee8783eb73684ab51aa7a16d509ec172
Vansh-ai12 <vj2754108@gmail.com>
2025-07-01 14:27:32 +0530

    Update Login.js

--- Server/Routes/Login.js
+++ Server/Routes/Login.js
@@ -29,7 +29,8 @@ router.post("/",async(req,res)=>{
     res.cookie("token",token,{
         httpOnly:true,
         maxAge:7*24*60*60*1000,
-        sameSite:"None"
+        sameSite:"None",
+        secure:true
     });
     return res.status(201).json({msg:"Success"});
 
